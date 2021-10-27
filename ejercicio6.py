69c3942856e50fbd550f5ea54ce46d4bdcf0becf
Sbastdia <sbastdia@myuax.com>
2021-10-27 23:25:01 +0200

    ejercicio 6 corregido

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,12 +1,12 @@
 
 
-a = int ( input ())
-b = int ( input ())
+a = int ( input ('Introduce un primer número: '))
+b = int ( input ('Introduce un segundo número: '))
 
-print('Suma: ' + a+b)
-print('Resta: ' + a-b)
-print('Multiplicación: ' + a*b)
-print('División de enteros: ' + a//b)
-print('División flotante: ' + a/b)
+print('Suma: ', a+b)
+print('Resta: ', a-b)
+print('Multiplicación: ', a*b)
+print('División de enteros: ', a//b)
+print('División flotante: ', a/b)
 
 
