fd52c5823815a1e3ea647770efff274959d7daed
Sbastdia <sbastdia@myuax.com>
2021-10-27 19:44:02 +0200

    ejercicio 6

---
+++ ejercicio6.py
@@ -0,0 +1,12 @@
+
+
+a = int ( input ())
+b = int ( input ())
+
+print('Suma: ' + a+b)
+print('Resta: ' + a-b)
+print('Multiplicación: ' + a*b)
+print('División de enteros: ' + a//b)
+print('División flotante: ' + a/b)
+
+
