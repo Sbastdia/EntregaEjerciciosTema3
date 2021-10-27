90a9ed19f40f747ce78b598d0d03c5a3c7b85513
Sbastdia <sbastdia@myuax.com>
2021-10-27 23:16:58 +0200

    ejercicio 8 corregido

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,16 +1,32 @@
 from random import randint
 
-numAleatorio1= randint(1, 15)
+print('Primera parte del ejercicio')
 
-a= int(input())
+numAleatorio1= randint(1, 15)
+a= int(input('Escribe un número: '))
 while a!=numAleatorio1:
     if(a<numAleatorio1):
         print('El número que debe adivinar es mayor')
     elif(a>numAleatorio1):
         print('El número que debe adivinar es menor')
-    a= int(input())
+    a= int(input('Escribe un número: '))
 
 print('¡ENHORABUENA, HAS ENCONTRADO EL NÚMER0!')
 
+print('Segunda parte del ejercicio')
 
+numAleatorio2= randint(1, 300)
+i=0
+b=0
+while b!=numAleatorio2 and i<9:
+    b= int(input("Escribe un número: "))
+    if(b<numAleatorio2):
+        print('El número que debe adivinar es mayor')
+    elif(b>numAleatorio2):
+        print('El número que debe adivinar es menor')
+    i=i+1
 
+if(b==numAleatorio2):
+    print('¡ENHORABUENA, HAS ENCONTRADO EL NÚMER0!')
+else:
+    print('Lo siento, has pasado el número de intentos')
